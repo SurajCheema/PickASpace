091dd76c556835e427660680eccd8c0af360d2ce
ssc26 <ssc26@student.le.ac.uk>
2024-01-19 15:55:22 +0000

    Final touches to the backend setup. Starting register page.

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const db = require('./models'); 
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello World! PickASpace is currently in development...');
+});
+
+// Start the server (type 'node server.js')
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
+});
+
+app.post('/create-user', async (req, res) => {
+  try {
+    const newUser = await db.User.create(req.body);
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
