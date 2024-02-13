da3c7fa254cbe2b14419dc82049a2ce76b83e1f3
ssc26 <ssc26@student.le.ac.uk>
2024-02-13 19:10:04 +0000

    Create initial navbar

---
+++ pickaspace-front-end/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+      <!-- Navigation Links -->
+      <router-link to="/">Home</router-link>
+      <!-- More links can be added here -->
+  
+      <!-- Logout Button -->
+      <button v-if="isLoggedIn" @click="logout">Logout</button>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+    computed: {
+      isLoggedIn() {
+        // Check if user is logged in by checking for a token
+        return !!localStorage.getItem('token');
+      }
+    },
+    methods: {
+      logout() {
+        // Remove token from localStorage
+        localStorage.removeItem('token');
+        // Redirect to login page or home page
+        this.$router.push('/login');
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+    background-color: #f5f5f5;
+  }
+  
+  nav a {
+    margin-right: 1rem;
+  }
+  </style>
+  
