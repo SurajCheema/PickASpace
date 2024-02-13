c8a2450103925c312f9d196416bdfb8ce47c5481
ssc26 <ssc26@student.le.ac.uk>
2024-02-13 20:11:17 +0000

    Logout button working and added to navbar

--- pickaspace-front-end/src/App.vue
+++ pickaspace-front-end/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <MainNavbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MainNavbar from './components/MainNavbar'
 
 export default {
   name: 'App',
   components: {
+    MainNavbar
   }
 }
 </script>
---
+++ pickaspace-front-end/src/components/MainNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">PickASpace</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <!-- Your navigation items here -->
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item" v-if="isLoggedIn">
+          <a class="nav-link" href="#" @click.prevent="logout">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  computed: {
+    isLoggedIn() {
+      return !!localStorage.getItem('token');
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      // Additional logout logic here
+      this.$router.push('/login');
+      alert('You have been logged out successfully.');
+    }
+  }
+}
+</script>
+
+<style>
+/* Additional styles if needed */
+</style>
