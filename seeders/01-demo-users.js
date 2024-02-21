a7205f835c3111dcb5bbf2795d52222af6dd84ed
ssc26 <ssc26@student.le.ac.uk>
2024-02-21 22:59:48 +0000

    Updated Users table seeder to hash and salt the password

--- seeders/01-demo-users.js
+++ seeders/01-demo-users.js
@@ -1,18 +1,28 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [
+    const saltRounds = 10; // Same as register process
+    const users = [
       {
         email: 'user@example.com',
-        password: 'hashedpassword', // Will hash in production
+        password: 'password', // Plain password will be hashed below
         full_name: 'John Doe',
         DOB: new Date('1990-01-01'),
         car_registration: 'ABC123',
         createdAt: new Date(),
         updatedAt: new Date()
       }
-    ], {});
+    ];
+
+    // Hash password 
+    for (let user of users) {
+      user.password = await bcrypt.hash(user.password, saltRounds);
+    }
+
+    await queryInterface.bulkInsert('Users', users, {});
   },
 
   async down (queryInterface, Sequelize) {
