677e5359050a33e92e1542fc5a3e5317b7e126a8
ssc26 <ssc26@student.le.ac.uk>
2024-02-21 22:54:46 +0000

    Seeder data updated for CarParkLogs table

--- seeders/05-demo-carparklogs.js
+++ seeders/05-demo-carparklogs.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const hourlyRate = 10; // Hourly rate
+    const durationHours = 2; // Duration in hours
+    const cost = hourlyRate * durationHours; // Calculate total cost based on duration and hourly rate
+
     await queryInterface.bulkInsert('CarParkLogs', [
       {
         carpark_id: 1,
@@ -9,7 +13,8 @@ module.exports = {
         bay_id: 1,
         payment_id: 1, 
         startTime: new Date(),
-        endTime: new Date(new Date().getTime() + (2*60*60*1000)), // 2 hours later
+        endTime: new Date(new Date().getTime() + (durationHours*60*60*1000)), // 2 hours later
+        cost: cost,
         createdAt: new Date(),
         updatedAt: new Date()
       }
