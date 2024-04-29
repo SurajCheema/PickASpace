8447c0936a92e4962990ed01a7e693a63b6a2727
ssc26 <ssc26@student.le.ac.uk>
2024-04-29 22:41:41 +0100

    Initial Admin Dashboard page with working button to the refunds management page

--- pickaspace-front-end/src/views/AdminDashboard.vue
+++ pickaspace-front-end/src/views/AdminDashboard.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
+    <div class="admin-dashboard">
       <h1>Admin Dashboard</h1>
-      <p>Hello World</p>
+      <b-button variant="primary" @click="goToRefunds">Manage Refunds</b-button>
     </div>
   </template>
   
   <script>
+  import { BButton } from 'bootstrap-vue-next';
+  
   export default {
-    name: 'AdminDashboard'
+    name: 'AdminDashboard',
+    components: {
+      BButton
+    },
+    methods: {
+      goToRefunds() {
+        this.$router.push({ name: 'AdminRefundRequests' });
+      }
+    }
   }
   </script>
   
+  <style scoped>
+  .admin-dashboard {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  </style>
